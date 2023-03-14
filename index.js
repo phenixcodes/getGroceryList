3979dd1db4fa37ba0682585d86dfa97de831b29e
Phenix <123134810+phenixcodes@users.noreply.github.com>
2023-03-14 04:25:55 +0000

    Follow up commit to clean up any syntax errors and formatting in this code.

--- index.js
+++ index.js
@@ -9,56 +9,70 @@ var groceryList = groceryKey(userName,userInput);
 
 //the groceryKey function validates if each grocery item is correct and saves the data to an array called groceryList
 function groceryKey(userName,userInput){
-    //making the userInput accessible to all functions
-    var getList = userInput;
+//making the userInput accessible to all functions
+var getList = userInput;
 
-    //will exit the program if given an empty or null string
-    if (userName != "" && userName !=" "){
-            
-                //check that it's a valid string
-				if ((typeof listItem) === "string" && listItem != ""){ 
-					newUser.push(listItem);
-					i++;
-				}
-                    
-                //exit loop upon invalid string
-				else{
-					return "'" + listItem + "'" + " is not a valid input. Try again";
-				}
+//will exit the program if given an empty or null string
+if (userName != "" && userName !=" "){
+
+    //if input is valid with username and less than 15 items
+	if ((typeof getList) == "number" && getList < 15){
+
+        //initializing the variables
+		var i = 0;
+		var listAmount = getList;
+		var newUser = new Array();
+
+        //add as many list items as needed
+		while (i < listAmount){
+			var listItem = prompt("Enter a grocery item:");
+
+            //check that it's a valid string
+			if ((typeof listItem) === "string" && listItem != ""){
+				newUser.push(listItem);
+				i++;
+			}
+
+            //exit loop upon invalid string
+			else{
+				return "'" + listItem + "'" + " is not a valid input. Try again";
+			}
 		}
         //making sure the list is completed before printing the user's data
 		if (i === listAmount){
-                var counter = 1;
+            var counter = 1;
+
+            console.log("\n" + "Howdy, " + userName + "!" + "\n");
+            console.log("Here's your grocery list !");
             
-    		newUser.forEach(function(entry){
-                switch (entry) {
-                case userName : console.log("\n" + "Howdy, " + entry + "!" + "\n");
-                    break;
-                case "grocery list" : console.log("Here's your " + entry + " !");
-                    break;
-                default: console.log(counter + ". " + entry);
-                    counter++;
+            //looping through the array newUser to read each entry item to the user in list format
+			newUser.forEach(function(entry){
+
+                console.log(counter + ". " + entry);
+                counter++;
+
+            });
+            //this reads on a new line
+            console.log();
+		    console.log("Don't forget your list at home! Happy shopping!");
+		}
+	}
+    //If too many items, then exit
+	else if (getList > 15){
+		return "Grocery list cannot be longer than 15 items. Try Again.";
+	}
+    //invalid input exits the function
+	else{
+		return "Invalid Input. Try Again.";
+	}
+}
+//userName will be the tokent to pull user data later on
+else{
+    return "You must create a username. Please Try again. "
+}
     
-                });
-                //this reads on a new line
-                console.log();
-    		    console.log("Don't forget your list at home! Happy shopping!");
-    		}
-    	}
-        //If too many items, then exit
-    	else if (getList > 15){
-    		return "Grocery list cannot be longer than 15 items. Try Again.";
-    	}
-        //invalid input exits the function
-    	else{
-    		return "Invalid Input. Try Again.";
-    	}
-    }
-    //userName will be the tokent to pull user data later on         
-    else{
-        return "You must create a username. Please Try again. "
-    }
-    return newUser;
+return newUser;
+
 }
 
 function createUser(userName,passWord,groceryList){
@@ -69,12 +83,10 @@ function createUser(userName,passWord,groceryList){
         "groceryList" : groceryList,
     }
     return keyChain;
+
 }
 
 var keyChain = createUser(userName, passWord, groceryList);
-
-//prints userName & groceryList out to the user.
-console.log("\n" + groceryList);
 
 /* this is a test to check what items are stored in the object keyChain
 console.log(keyChain);*/
